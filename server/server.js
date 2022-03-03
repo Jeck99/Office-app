b3a9bf1f10c0ef8d3ba3eece37cdb6c8bcc3a614
jeck99 <jeck99@gmail.com>
2022-03-03 13:32:16 +0200

    aa

--- server/server.js
+++ server/server.js
@@ -6,19 +6,17 @@ const employeeRouter = require('./routes/employees-route');
 const userRouter = require('./routes/user-route');
 const passport = require("passport");
 const path = require('path');
-
-const passportConfig =require("./config/passport");
-passportConfig(passport);
+require("./config/passport")(passport);
 
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// app.use(passport.initialize());
+app.use(passport.initialize());
 app.use('/auth', userRouter);
-// app.use('/employees', passport.authenticate('jwt', { session: false }), employeeRouter);
-// app.get('/', (req, res) => { res.send('Hello World') });
+app.use('/employees', passport.authenticate('jwt', { session: false }), employeeRouter);
+
 app.listen(process.env.PORT);
 //*****************************************************************/
 if (process.env.NODE_ENV === 'production') {
