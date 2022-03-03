8906a61ef39226cebfb5d83bdfe0545fd1b4ace2
jeck99 <jeck99@gmail.com>
2022-03-03 13:15:27 +0200

    r

--- server/server.js
+++ server/server.js
@@ -6,25 +6,25 @@ const employeeRouter = require('./routes/employees-route');
 const userRouter = require('./routes/user-route');
 const passport = require("passport");
 const path = require('path');
-require("./config/passport")(passport);
+// require("./config/passport")(passport);
 
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(passport.initialize());
+// app.use(passport.initialize());
 app.use('/auth', userRouter);
-app.use('/employees', passport.authenticate('jwt', { session: false }), employeeRouter);
-app.get('/', (req, res) => { res.send('Hello World') });
+// app.use('/employees', passport.authenticate('jwt', { session: false }), employeeRouter);
+// app.get('/', (req, res) => { res.send('Hello World') });
 app.listen(process.env.PORT);
 //*****************************************************************/
-// if (process.env.NODE_ENV === 'production') {
-//     // Serve any static files
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-//     // Handle React routing, return all requests to React app
-//     app.get('/', (req, res)=>{
-//         res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-//     });
-//   }
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('/', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
 //*******************************************************************/
